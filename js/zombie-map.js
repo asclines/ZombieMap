085b6e5c58f87c29945b919d5d53df9395cf42cc
asclines <injpk23@gmail.com>
2017-01-23 17:20:04 -0600

    Fixed issue with hovering over states that resulted from removing the tabs

--- js/zombie-map.js
+++ js/zombie-map.js
@@ -24,18 +24,8 @@ function setupMap() {
               }]
     },
     onRegionTipShow: function(event, label, code) {
-      if(document.getElementById('status-setparams').classList.contains("active")) {
-        label.html(
-          '<b>' + label.html() + '</b></br>' +
-          '<b>Population: </b>' + Number(zombieMapData.data.humanpop[zombieMapData.val][code]).toLocaleString()
-        );
-      } else if(document.getElementById('status-setmap').classList.contains("active")) {
-        label.html(
-          '<b>' + label.html() + '</b></br>' +
-          '<b>Population: </b>' + Number(zombieMapData.data.humanpop[zombieMapData.val][code]).toLocaleString() + '</b></br>' +
-          '<b>Initial Zombies </b>' + zombieMapData.data.percentage[zombieMapData.val][code] + '%'
-        );
-      } else if(document.getElementById('status-calc').classList.contains("active")) {
+
+      if(document.getElementById('div-runtime').classList.contains("active")) {
         label.html(
           '<b>' + label.html() + '</b></br>' +
           '<b>Zombie takeover: </b>' + zombieMapData.data.percentage[zombieMapData.val][code] + '% </b></br>' +
@@ -43,8 +33,13 @@ function setupMap() {
           '<b>Zombie Population: </b>' + Number(zombieMapData.data.zombiepop[zombieMapData.val][code]).toLocaleString()
 
         );
+      } else {
+        label.html(
+          '<b>' + label.html() + '</b></br>' +
+          '<b>Population: </b>' + Number(zombieMapData.data.humanpop[zombieMapData.val][code]).toLocaleString() + '</b></br>' +
+          '<b>Initial Zombies </b>' + zombieMapData.data.percentage[zombieMapData.val][code] + '%'
+        );
       }
-
     },
     onRegionClick: function(event, code) {
       //Only increment if the "Set Map" tab is loaded
