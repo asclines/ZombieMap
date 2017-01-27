9921ab9d98fe99afc39417a6bbedfedb0d7d6a94
asclines <injpk23@gmail.com>
2017-01-27 14:59:43 -0600

    Setup getting initial data

--- zombies.js
+++ zombies.js
@@ -1,8 +1,17 @@
+log.setLevel('trace'); //See https://github.com/pimterry/loglevel for more
 
 zombies = {
   inProgress: false, //Various parts of the elements in this site depend on this fact.
-
+  currentTime: 0,
+  init: function() {
+    log.debug("Initializing zombies")
+    this.getInitialData(this.setupMap);
+  },
+  /*
+  MAP HANDLING
+  */
   setupMap: function() {
+    log.debug("Setting up map")
     this.currentTime = 0;
     this.mapObject = new jvm.Map({
       container: $('#map'),
@@ -12,7 +21,7 @@ zombies = {
         regions: [{
           scale: ['#99ff99', '#990000'],
           attribute: 'fill',
-        //  values: this.data.percentage[zombieSim.currentTime],
+          values: zombies.stateData.percentage[zombies.currentTime],
           min: 0,
           max: 100,
           legend: {
@@ -22,5 +31,50 @@ zombies = {
           }]
       }
     });
+  }, //End - setupMap
+
+
+
+
+  /*
+  DATA HANDLING
+  */
+
+  getInitialData: function(done) {
+    log.debug("Loading data files")
+    $.when(
+      $.getJSON('data/states-initial.json'),
+      $.getJSON('data/states-pops.json')
+    ).done(function(
+      statesInitial,
+      statesPopulation
+    ) {
+      log.debug("Preparing data")
+      var initialStatePercentage = JSON.parse(JSON.stringify(statesInitial));
+      var initialStatesZombiePopulation = JSON.parse(JSON.stringify(statesInitial));
+
+      var stateData = {
+        "percentage": {
+          "0": initialStatePercentage["0"]
+        },
+        "humanpop": {
+          "0": statesPopulation["0"]
+        },
+        "zombiepop": {
+          "0": initialStatesZombiePopulation["0"]
+        }
+      }
+      zombies.stateData = stateData;
+
+
+      log.debug("Done preparing data")
+      done();
+    });
+
   }
-}
+
+
+
+
+
+} //End - zombies
