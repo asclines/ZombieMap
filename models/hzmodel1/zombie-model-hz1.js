ea480613997c8d38dfea8f236d7a9522b595ac52
asclines <injpk23@gmail.com>
2017-02-01 15:23:16 -0600

    Added bite results params

--- models/hzmodel1/zombie-model-hz1.js
+++ models/hzmodel1/zombie-model-hz1.js
@@ -5,10 +5,29 @@ zombieModel = {
 
   setup: function(){
     zombieModel.params = {  //With default values
-      biteRate: 10
-
+      biteRate: 10,
+      biteInfectsChance: 20
     };
 
+    // Handling Bite Infection or Kill
+    document.getElementById('biteInfects').innerHTML = this.params.biteInfectsChance;
+    document.getElementById('biteKills').innerHTML = 100 - this.params.biteInfectsChance;
+
+    $("#slider-bite-infects").slider({
+      value: zombieModel.params.biteInfectsChance,
+      min: 0,
+      max: 100,
+      step: 1,
+      slide: function(event, ui){
+        zombieModel.params.biteInfectsChance = ui.value;
+        document.getElementById('biteInfects').innerHTML = zombieModel.params.biteInfectsChance;
+        document.getElementById('biteKills').innerHTML = 100 - zombieModel.params.biteInfectsChance;
+      }
+    });
+
+
+
+    // Handling Bite Rate
     document.getElementById('biteRate').innerHTML = this.params.biteRate;
     $("#slider-bite-rate").slider({
       value: zombieModel.params.biteRate,
@@ -20,6 +39,7 @@ zombieModel = {
         document.getElementById('biteRate').innerHTML = zombieModel.params.biteRate;
       }
     });
+
   },
 
   nextIteration: function(population, neighbors) {
