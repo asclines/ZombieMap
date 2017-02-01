7adfef56ef250d7db0ecc1cf5efaaaa976a7c35e
asclines <injpk23@gmail.com>
2017-02-01 15:12:57 -0600

    Added skeleton model js code

---
+++ models/hzmodel1/zombie-model-hz1.js
@@ -0,0 +1,15 @@
+
+zombieModel = {
+
+  paramSettingsHtmlFile: "models/sample/zombie-model-hz1.html",
+
+  setup: function(){
+    zombieModel.params = {  //With default values
+
+    };
+  },
+
+  nextIteration: function(population, neighbors) {
+
+  }
+};
