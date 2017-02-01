8a72ed2398e7930d75052762c953b785d3a85acd
asclines <injpk23@gmail.com>
2017-02-01 15:28:21 -0600

    Added encounter rate

--- models/hzmodel1/zombie-model-hz1.js
+++ models/hzmodel1/zombie-model-hz1.js
@@ -6,7 +6,9 @@ zombieModel = {
   setup: function(){
     zombieModel.params = {  //With default values
       biteRate: 10,
-      biteInfectsChance: 20
+      biteInfectsChance: 20,
+      zombieDeathRate: 10,
+      encounterRate: 25
     };
 
     // Handling Bite Infection or Kill
@@ -25,8 +27,6 @@ zombieModel = {
       }
     });
 
-
-
     // Handling Bite Rate
     document.getElementById('biteRate').innerHTML = this.params.biteRate;
     $("#slider-bite-rate").slider({
@@ -37,6 +37,32 @@ zombieModel = {
       slide: function(event, ui){
         zombieModel.params.biteRate = ui.value;
         document.getElementById('biteRate').innerHTML = zombieModel.params.biteRate;
+      }
+    });
+
+    // Handling Humans Killing Zombies
+    document.getElementById('zombieDeathRate').innerHTML = this.params.zombieDeathRate;
+    $("#slider-zombie-death-rate").slider({
+      value: zombieModel.params.zombieDeathRate,
+      min: 0,
+      max: 100,
+      step: 1,
+      slide: function(event, ui){
+        zombieModel.params.zombieDeathRate = ui.value;
+        document.getElementById('zombieDeathRate').innerHTML = zombieModel.params.zombieDeathRate;
+      }
+    });
+
+    // Handling Humans running into Zombies
+    document.getElementById('encounterRate').innerHTML = this.params.encounterRate;
+    $("#slider-encounter-rate").slider({
+      value: zombieModel.params.encounterRate,
+      min: 0,
+      max: 100,
+      step: 1,
+      slide: function(event, ui){
+        zombieModel.params.encounterRate = ui.value;
+        document.getElementById('encounterRate').innerHTML = zombieModel.params.encounterRate;
       }
     });
 
